dffa4998fe89d7bbab7c9c541328ac94056b4da5
Brandon Croft <brandon@kapost.com>
2017-01-17 15:34:16 -0700

    Fixes false positive if previous platform command succeeded
    If you called a platform command that succeeded, that success would
    occlude future failures that do not use platform commands.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+if ENV["CIRCLE_ARTIFACTS"]
+  require "simplecov"
+  dir = File.join(ENV["CIRCLE_ARTIFACTS"], "coverage")
+  SimpleCov.coverage_dir(dir)
+  SimpleCov.start "rails"
+end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'kapost/bootstrapper'
